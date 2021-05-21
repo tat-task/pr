b26f79f9b5c4a0c8b1badf5c10747d0dcfa7b260
Kisielius <antanas.kisielius@seb.se>
2021-05-21 12:26:27 +0300

    2

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,31 +1,11 @@
 let homepage = function () {
 
     //define your elements to be later used in a functions
-    let firstNumber_input = element(by.model('first'));
-    let secondNumber_input = element(by.model('second'));
-    let goButton = element(by.css('[ng-click="doAddition()"]'));
+    let pimLink = element(by.id('menu_pim_viewPimModule'));
 
-    this.get = function (url) {
-        browser.get(url);
-    }
-
-    this.enterFirstNumber = function (firstNo) {
-        firstNumber_input.sendKeys(firstNo);
+    this.clickPIM = function () {
+        pimLink.click();
     };
-
-    this.enterSecondNumber = function (secondNo) {
-        secondNumber_input.sendKeys(secondNo);
-    };
-
-    this.clickGo = function () {
-        goButton.click();
-    };
-
-    this.verifyResult = function (result) {
-        let output = element(by.cssContainingText('.ng-binding', result));
-        expect(output.getText()).toEqual(result);
-    }
-
 };
 
 module.exports = new homepage();
